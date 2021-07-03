9f240f67007bd0da830f28fce176e1e7a4e2a829
iqbalhumam73 <iqbalhumam73@gmail.com>
2021-07-03 21:27:27 +0700

    updeetttt

--- application/views/web/pages/payment.php
+++ application/views/web/pages/payment.php
@@ -1,12 +1,10 @@
-
-
 <div class="main">
     <div class="content" style="text-align: center">
         <div class="register_account" style="text-align:center;display:inline-block;float: none">
-            <h3>Payment Options</h3>
-            <p>Your Order Sucessfully Done.</p>
-           
-        </div>  	
+            <h3>Pilihan Pembayaran</h3>
+            <p>Pesananmu berhasil</p>
+
+        </div>
         <div class="clear"></div>
     </div>
 </div>
