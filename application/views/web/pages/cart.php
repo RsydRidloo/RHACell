3c45fb066c3059e47f16764cfe6b5cb7969d26f6
iqbalhumam73 <iqbalhumam73@gmail.com>
2021-07-03 20:59:05 +0700

    Update cart.php

--- application/views/web/pages/cart.php
+++ application/views/web/pages/cart.php
@@ -1,43 +1,41 @@
-
-
 <div class="main">
     <div class="content">
-        <div class="cartoption">		
+        <div class="cartoption">
             <div class="cartpage">
-                <h2>Your Cart</h2>
+                <h2>Keranjang Anda</h2>
                 <?php if ($this->cart->total_items()) { ?>
                     <table class="tblone">
                         <tr>
-                            <th width="5%">Sr.</th>
-                            <th width="30%">Product Name</th>
-                            <th width="10%">Image</th>
-                            <th width="15%">Price</th>
-                            <th width="20%">Quantity</th>
-                            <th width="15%">Total Price</th>
-                            <th width="5%">Remove</th>
+                            <th width="5%">No</th>
+                            <th width="30%">Nama Produk</th>
+                            <th width="10%">Gambar</th>
+                            <th width="15%">Harga</th>
+                            <th width="20%">Jumlah</th>
+                            <th width="15%">Total Harga</th>
+                            <th width="5%">Hapus</th>
                         </tr>
                         <?php
                         $i = 0;
                         foreach ($cart_contents as $cart_items) {
                             $i++;
-                            ?>
+                        ?>
                             <tr>
                                 <td><?php echo $i; ?></td>
                                 <td><?php echo $cart_items['name'] ?></td>
-                                <td><img src="<?php echo base_url('uploads/' . $cart_items['options']['product_image']) ?>" alt=""/></td>
+                                <td><img src="<?php echo base_url('uploads/' . $cart_items['options']['product_image']) ?>" alt="" /></td>
                                 <td>Rp<?php echo $this->cart->format_number($cart_items['price']) ?></td>
                                 <td>
                                     <form action="<?php echo base_url('update/cart'); ?>" method="post">
-                                        <input type="number" name="qty" value="<?php echo $cart_items['qty'] ?>"/>
-                                        <input type="hidden" name="rowid" value="<?php echo $cart_items['rowid'] ?>"/>
-                                        <input type="submit" name="submit" value="Update"/>
+                                        <input type="number" name="qty" value="<?php echo $cart_items['qty'] ?>" />
+                                        <input type="hidden" name="rowid" value="<?php echo $cart_items['rowid'] ?>" />
+                                        <input type="submit" name="submit" value="Update" />
                                     </form>
                                 </td>
                                 <td>Rp<?php echo $this->cart->format_number($cart_items['subtotal']) ?></td>
                                 <td>
                                     <form action="<?php echo base_url('remove/cart'); ?>" method="post">
-                                        <input type="hidden" name="rowid" value="<?php echo $cart_items['rowid'] ?>"/>
-                                        <input type="submit" name="submit" value="X"/>
+                                        <input type="hidden" name="rowid" value="<?php echo $cart_items['rowid'] ?>" />
+                                        <input type="submit" name="submit" value="X" />
                                     </form>
                                 </td>
                             </tr>
@@ -52,7 +50,7 @@
                         </tr>
                         <tr>
                             <th>VAT : </th>
-                            <td>Rp 
+                            <td>Rp
                                 <?php
                                 $total = $this->cart->total();
                                 $tax = ($total * 15) / 100;
@@ -65,7 +63,7 @@
                             <td>Rp <?php echo $this->cart->format_number($tax + $this->cart->total()); ?> </td>
                         </tr>
                     </table>
-                    <?php
+                <?php
                 } else {
                     echo "<h1>Your Cart Empty</h1>";
                 }
@@ -79,18 +77,18 @@
                     <?php
                     $customer_id = $this->session->userdata('customer_id');
                     if (empty($customer_id)) {
-                        ?>
+                    ?>
                         <a href="<?php echo base_url('user_form') ?>"> <img src="<?php echo base_url() ?>assets/web/images/check.png" alt="" /></a>
-                        <?php
+                    <?php
                     } elseif (!empty($customer_id)) {
-                        ?>
-                        <a href = "<?php echo base_url('customer/shipping') ?>"> <img src = "<?php echo base_url() ?>assets/web/images/check.png" alt = "" /></a>
-                        <?php
+                    ?>
+                        <a href="<?php echo base_url('customer/shipping') ?>"> <img src="<?php echo base_url() ?>assets/web/images/check.png" alt="" /></a>
+                    <?php
                     }
                     ?>
                 </div>
             </div>
-        </div>  	
+        </div>
         <div class="clear"></div>
     </div>
 </div>
