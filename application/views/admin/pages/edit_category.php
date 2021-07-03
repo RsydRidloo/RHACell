430d51cd087e9d384d08c2ab9de62579a6b427b8
iqbalhumam73 <iqbalhumam73@gmail.com>
2021-07-03 22:11:25 +0700

    edit kategori

--- application/views/admin/pages/edit_category.php
+++ application/views/admin/pages/edit_category.php
@@ -5,19 +5,19 @@
     <ul class="breadcrumb">
         <li>
             <i class="icon-home"></i>
-            <a href="<?php echo base_url('dashboard')?>">Home</a>
-            <i class="icon-angle-right"></i> 
+            <a href="<?php echo base_url('dashboard') ?>">Home</a>
+            <i class="icon-angle-right"></i>
         </li>
         <li>
             <i class="icon-edit"></i>
-            <a href="<?php echo base_url('edit/category/'.$category_info_by_id->id)?>">Edit Category</a>
+            <a href="<?php echo base_url('edit/category/' . $category_info_by_id->id) ?>">Edit Kategori</a>
         </li>
     </ul>
 
     <div class="row-fluid sortable">
         <div class="box span12">
             <div class="box-header" data-original-title>
-                <h2><i class="halflings-icon edit"></i><span class="break"></span>Edit Category</h2>
+                <h2><i class="halflings-icon edit"></i><span class="break"></span>Edit Kategori</h2>
                 <div class="box-icon">
                     <a href="#" class="btn-setting"><i class="halflings-icon wrench"></i></a>
                     <a href="#" class="btn-minimize"><i class="halflings-icon chevron-up"></i></a>
@@ -25,55 +25,64 @@
                 </div>
             </div>
             <style type="text/css">
-                #result{color:red;padding: 5px}
-                #result p{color:red}
+                #result {
+                    color: red;
+                    padding: 5px
+                }
+
+                #result p {
+                    color: red
+                }
             </style>
             <div id="result">
-                <p><?php echo $this->session->flashdata('message');?></p>
+                <p><?php echo $this->session->flashdata('message'); ?></p>
             </div>
             <div class="box-content">
-                <form class="form-horizontal" action="<?php echo base_url('update/category/'.$category_info_by_id->id);?>" method="post">
+                <form class="form-horizontal" action="<?php echo base_url('update/category/' . $category_info_by_id->id); ?>" method="post">
                     <fieldset>
 
                         <div class="control-group">
-                            <label class="control-label" for="fileInput">Category Name</label>
+                            <label class="control-label" for="fileInput">Nama Kategori</label>
                             <div class="controls">
-                                <input class="span6 typeahead" value="<?php echo $category_info_by_id->category_name;?>" id="category_name" name="category_name" type="text"/>
+                                <input class="span6 typeahead" value="<?php echo $category_info_by_id->category_name; ?>" id="category_name" name="category_name" type="text" />
                             </div>
-                        </div>          
+                        </div>
                         <div class="control-group">
-                            <label class="control-label" for="textarea2">Category Description</label>
+                            <label class="control-label" for="textarea2">Deksripsi Singkat</label>
                             <div class="controls">
                                 <textarea class="cleditor" id="category_description" name="category_description" rows="3">
-                                    <?php echo $category_info_by_id->category_description;?>
+                                    <?php echo $category_info_by_id->category_description; ?>
                                 </textarea>
                             </div>
                         </div>
-                                
+
                         <div class="control-group">
-                            <label class="control-label" for="textarea2">Publication Status</label>
+                            <label class="control-label" for="textarea2">Status Publikasi</label>
                             <div class="controls">
                                 <select id="slect" name="publication_status">
-                                    <option value="1">Published</option>
-                                    <option value="0">UnPublished</option>
+                                    <option value="1">Sudah</option>
+                                    <option value="0">Belum</option>
                                 </select>
                             </div>
                         </div>
-                        
+
                         <div class="form-actions">
-                            <button type="submit" id="save_category" class="btn btn-primary">Save changes</button>
-                            <button type="reset" class="btn">Cancel</button>
+                            <button type="submit" id="save_category" class="btn btn-primary">Simpan</button>
+                            <button type="reset" class="btn">Batal</button>
                         </div>
                     </fieldset>
-                </form>   
+                </form>
 
             </div>
-        </div><!--/span-->
+        </div>
+        <!--/span-->
 
-    </div><!--/row-->
+    </div>
+    <!--/row-->
 
-   
-    
-</div><!--/.fluid-container-->
+
+
+</div>
+<!--/.fluid-container-->
 
 <!-- end: Content -->
