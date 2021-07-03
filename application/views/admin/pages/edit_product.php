85560ade83d067604dd2e02d2056dc4fe6fb4369
iqbalhumam73 <iqbalhumam73@gmail.com>
2021-07-03 23:02:28 +0700

    updet

--- application/views/admin/pages/edit_product.php
+++ application/views/admin/pages/edit_product.php
@@ -5,12 +5,12 @@
     <ul class="breadcrumb">
         <li>
             <i class="icon-home"></i>
-            <a href="<?php echo base_url('dashboard')?>">Home</a>
-            <i class="icon-angle-right"></i> 
+            <a href="<?php echo base_url('dashboard') ?>">Home</a>
+            <i class="icon-angle-right"></i>
         </li>
         <li>
             <i class="icon-edit"></i>
-            <a href="<?php echo base_url('edit/product/'.$product_info_by_id->product_id)?>">edit Product</a>
+            <a href="<?php echo base_url('edit/product/' . $product_info_by_id->product_id) ?>">edit Product</a>
         </li>
     </ul>
 
@@ -25,96 +25,102 @@
                 </div>
             </div>
             <style type="text/css">
-                #result{color:red;padding: 5px}
-                #result p{color:red}
+                #result {
+                    color: red;
+                    padding: 5px
+                }
+
+                #result p {
+                    color: red
+                }
             </style>
             <div id="result">
-                <p><?php echo $this->session->flashdata('message');?></p>
+                <p><?php echo $this->session->flashdata('message'); ?></p>
             </div>
             <div class="box-content">
-                <form name="formName" class="form-horizontal" action="<?php echo base_url('update/product/'.$product_info_by_id->product_id);?>" method="post" enctype="multipart/form-data">
+                <form name="formName" class="form-horizontal" action="<?php echo base_url('update/product/' . $product_info_by_id->product_id); ?>" method="post" enctype="multipart/form-data">
                     <fieldset>
 
                         <div class="control-group">
                             <label class="control-label" for="fileInput">Product Title</label>
                             <div class="controls">
-                                <input class="span6 typeahead" value="<?php echo $product_info_by_id->product_title;?>" name="product_title" id="fileInput" type="text"/>
+                                <input class="span6 typeahead" value="<?php echo $product_info_by_id->product_title; ?>" name="product_title" id="fileInput" type="text" />
                             </div>
-                        </div>          
+                        </div>
                         <div class="control-group">
                             <label class="control-label" for="textarea2">Product Short Description</label>
                             <div class="controls">
                                 <textarea class="cleditor" name="product_short_description" id="textarea2" rows="2">
-                                    <?php echo $product_info_by_id->product_short_description;?>
+                                    <?php echo $product_info_by_id->product_short_description; ?>
                                 </textarea>
                             </div>
-                        </div>        
+                        </div>
                         <div class="control-group">
                             <label class="control-label" for="textarea2">Product Long Description</label>
                             <div class="controls">
                                 <textarea class="cleditor" name="product_long_description" id="textarea2" rows="4">
-                                    <?php echo $product_info_by_id->product_long_description;?>
+                                    <?php echo $product_info_by_id->product_long_description; ?>
                                 </textarea>
                             </div>
                         </div>
                         <div class="control-group">
                             <label class="control-label" for="fileInput">Product Image</label>
                             <div class="controls">
-                                <input class="span6 typeahead" name="product_image" id="fileInput" type="file"/>
-                                <input class="span6 typeahead" name="product_delete_image" value="<?php echo base_url('uploads/'.$product_info_by_id->product_image);?>" type="hidden"/>
+                                <input class="span6 typeahead" name="product_image" id="fileInput" type="file" />
+                                <input class="span6 typeahead" name="product_delete_image" value="<?php echo base_url('uploads/' . $product_info_by_id->product_image); ?>" type="hidden" />
                             </div>
                         </div>
-                        
+
                         <div class="control-group">
                             <div class="controls">
-                                <img src="<?php echo base_url('uploads/'.$product_info_by_id->product_image);?>" style="width:500px;height:200px"/>
+                                <img src="<?php echo base_url('uploads/' . $product_info_by_id->product_image); ?>" style="width:500px;height:200px" />
                             </div>
-                        </div> 
-                        
+                        </div>
+
                         <div class="control-group">
                             <label class="control-label" for="fileInput">Product Price</label>
                             <div class="controls">
-                                <input class="span6 typeahead" value="<?php echo $product_info_by_id->product_price;?>" name="product_price" id="fileInput" type="text"/>
+                                <input class="span6 typeahead" value="<?php echo $product_info_by_id->product_price; ?>" name="product_price" id="fileInput" type="text" />
                             </div>
                         </div>
-                        
+
                         <div class="control-group">
                             <label class="control-label" for="fileInput">Product Quantity</label>
                             <div class="controls">
-                                <input class="span6 typeahead" value="<?php echo $product_info_by_id->product_quantity;?>" name="product_quantity" id="fileInput" type="text"/>
+                                <input class="span6 typeahead" value="<?php echo $product_info_by_id->product_quantity; ?>" name="product_quantity" id="fileInput" type="text" />
                             </div>
                         </div>
-                        
+
                         <div class="control-group">
                             <label class="control-label" for="fileInput">Product Category</label>
                             <div class="controls">
                                 <select id="product_category" name="product_category">
-                                    <?php foreach($all_published_category as $single_category){?>
-                                    <option value="<?php echo $single_category->id;?>"><?php echo $single_category->category_name;?></option>
-                                    <?php }?>
+                                    <?php foreach ($all_published_category as $single_category) { ?>
+                                        <option value="<?php echo $single_category->id; ?>"><?php echo $single_category->category_name; ?></option>
+                                    <?php } ?>
                                 </select>
                             </div>
-                        </div> 
-                        
+                        </div>
+
                         <div class="control-group">
                             <label class="control-label" for="fileInput">Product Brand</label>
                             <div class="controls">
                                 <select id="product_brand" name="product_brand">
-                                    <?php foreach($all_published_brand as $single_brand){?>
-                                    <option value="<?php echo $single_brand->brand_id;?>"><?php echo $single_brand->brand_name;?></option>
-                                    <?php }?>
+                                    <?php foreach ($all_published_brand as $single_brand) { ?>
+                                        <option value="<?php echo $single_brand->brand_id; ?>"><?php echo $single_brand->brand_name; ?></option>
+                                    <?php } ?>
                                 </select>
                             </div>
-                        </div> 
-                        
+                        </div>
+
                         <div class="control-group">
                             <label class="control-label" for="fileInput">Product Featured</label>
                             <div class="controls">
-                                <input class="span6 typeahead" value="0" name="product_feature" id="fileInput" type="radio"/> Unfeatured
+                                <input class="span6 typeahead" value="0" name="product_feature" id="fileInput" type="radio" /> Unfeatured
                                 <input class="span6 typeahead" value="1" name="product_feature" id="fileInput" type="radio" />Featured
                             </div>
                         </div>
-                        
+
                         <div class="control-group">
                             <label class="control-label" for="textarea2">Publication Status</label>
                             <div class="controls">
@@ -124,27 +130,30 @@
                                 </select>
                             </div>
                         </div>
-                        
+
                         <div class="form-actions">
                             <button type="submit" class="btn btn-primary">Save changes</button>
                             <button type="reset" class="btn">Cancel</button>
                         </div>
                     </fieldset>
-                </form>   
+                </form>
 
             </div>
-        </div><!--/span-->
+        </div>
+        <!--/span-->
 
-    </div><!--/row-->
+    </div>
+    <!--/row-->
 
 
-</div><!--/.fluid-container-->
+</div>
+<!--/.fluid-container-->
 
 <!-- end: Content -->
 
 <script>
-document.getElementById('publication_status').value = <?php echo $product_info_by_id->pstatus;?>;
-document.formName.product_feature.value=<?php echo $product_info_by_id->product_feature;?>;
-document.getElementById('product_brand').value = <?php echo $product_info_by_id->product_brand;?>;
-document.getElementById('product_category').value = <?php echo $product_info_by_id->product_category;?>;
+    document.getElementById('publication_status').value = <?php echo $product_info_by_id->pstatus; ?>;
+    document.formName.product_feature.value = <?php echo $product_info_by_id->product_feature; ?>;
+    document.getElementById('product_brand').value = <?php echo $product_info_by_id->product_brand; ?>;
+    document.getElementById('product_category').value = <?php echo $product_info_by_id->product_category; ?>;
 </script>
--- application/views/admin/pages/pdf - Copy.php
+++ application/views/admin/pages/pdf - Copy.php
@@ -1,126 +1,141 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
-        <style type="text/css">
-            .header_area{padding:30px 0px;background:#4842f4;color:#fff}
-            .footer_area{padding:10px 0px;background:#4842f4;color:#fff}
-            .content_area{padding:30px 0px}
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="header_area">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <h1 align="center">Money Receipt</h1>
-                        </div>
+
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
+    <style type="text/css">
+        .header_area {
+            padding: 30px 0px;
+            background: #4842f4;
+            color: #fff
+        }
+
+        .footer_area {
+            padding: 10px 0px;
+            background: #4842f4;
+            color: #fff
+        }
+
+        .content_area {
+            padding: 30px 0px
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="header_area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 align="center">Nota Pembayaran</h1>
                     </div>
                 </div>
             </div>
+        </div>
 
-            <div class="content_area">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-5">
-                            <h2>Customer Info(00<?php echo $customer_info->customer_id; ?>)</h2>
-                            <table class="table table-hover table-bordered">
-                                <tr>
-                                    <td>Customer Name : </td>
-                                    <td><?php echo $customer_info->customer_name; ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Customer Adress : </td>
-                                    <td><?php echo $customer_info->customer_address; ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Customer Mobile : </td>
-                                    <td><?php echo $customer_info->customer_phone; ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Customer Email : </td>
-                                    <td><?php echo $customer_info->customer_email; ?></td>
-                                </tr>
-                            </table>
-                        </div>
-                        <div class="col-md-2">
-
-                        </div>
-                        <div class="col-md-5">
-                            <h2>Customer Info(00<?php echo $shipping_info->shipping_id; ?>)</h2>
-                            <table class="table table-hover table-bordered">
-                                <tr>
-                                    <td>Shpping Name : </td>
-                                    <td><?php echo $shipping_info->shipping_name; ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Shipping Adress : </td>
-                                    <td><?php echo $shipping_info->shipping_address; ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Shipping Mobile : </td>
-                                    <td><?php echo $shipping_info->shipping_phone; ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Shipping Email : </td>
-                                    <td><?php echo $shipping_info->shipping_email; ?></td>
-                                </tr>
-                            </table>
-                        </div>
+        <div class="content_area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-5">
+                        <h2>Informasi Pelanggan(00<?php echo $customer_info->customer_id; ?>)</h2>
+                        <table class="table table-hover table-bordered">
+                            <tr>
+                                <td>Nama Pelanggan :</td>
+                                <td><?php echo $customer_info->customer_name; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Alamat :</td>
+                                <td><?php echo $customer_info->customer_address; ?></td>
+                            </tr>
+                            <tr>
+                                <td>No Telepon :</td>
+                                <td><?php echo $customer_info->customer_phone; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Email :</td>
+                                <td><?php echo $customer_info->customer_email; ?></td>
+                            </tr>
+                        </table>
                     </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <table class="table table-striped table-bordered">
-                                <thead class="table-inverse">
+                    <div class="col-md-2">
+
+                    </div>
+                    <div class="col-md-5">
+                        <h2>Informasi Pelanggan(00<?php echo $shipping_info->shipping_id; ?>)</h2>
+                        <table class="table table-hover table-bordered">
+                            <tr>
+                                <td>Nama Belanja : </td>
+                                <td><?php echo $shipping_info->shipping_name; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Alamat Pengiriman : </td>
+                                <td><?php echo $shipping_info->shipping_address; ?></td>
+                            </tr>
+                            <tr>
+                                <td>No Telepon Pengiriman : </td>
+                                <td><?php echo $shipping_info->shipping_phone; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Email Pengiriman : </td>
+                                <td><?php echo $shipping_info->shipping_email; ?></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <table class="table table-striped table-bordered">
+                            <thead class="table-inverse">
+                                <tr>
+                                    <th>No </th>
+                                    <th>Nama Produk</th>
+                                    <th>Foto Produk</th>
+                                    <th>Harga</th>
+                                    <th>Stok</th>
+                                    <th>Subtotal</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $i = 0;
+                                foreach ($order_details_info as $single_order_details) {
+                                    $i++;
+                                ?>
                                     <tr>
-                                        <th>Sr.</th>
-                                        <th>Product Name</th>
-                                        <th>Product Image</th>
-                                        <th>Product Price</th>
-                                        <th>Product Qty</th>
-                                        <th>Product Subtotal</th>
+                                        <td><?php echo $i; ?></td>
+                                        <td><?php echo $single_order_details->product_name ?></td>
+                                        <td><img src="<?php echo base_url('uploads/' . $single_order_details->product_image); ?>" style="width:200px;height:100px" /></td>
+                                        <td><?php echo $single_order_details->product_price ?></td>
+                                        <td><?php echo $single_order_details->product_sales_quantity ?></td>
+                                        <td><?php echo $single_order_details->product_price * $single_order_details->product_sales_quantity ?></td>
                                     </tr>
-                                </thead>   
-                                <tbody>
-                                    <?php
-                                    $i = 0;
-                                    foreach ($order_details_info as $single_order_details) {
-                                        $i++;
-                                        ?>
-                                        <tr>
-                                            <td><?php echo $i; ?></td>
-                                            <td><?php echo $single_order_details->product_name ?></td>
-                                            <td><img src="<?php echo base_url('uploads/' . $single_order_details->product_image); ?>" style="width:200px;height:100px"/></td>
-                                            <td><?php echo $single_order_details->product_price ?></td>
-                                            <td><?php echo $single_order_details->product_sales_quantity ?></td>
-                                            <td><?php echo $single_order_details->product_price * $single_order_details->product_sales_quantity ?></td>
-                                        </tr>
-                                    <?php } ?>
-                                </tbody>
-                                <tfoot class="table-inverse">
-                                <td>Total Amount</td>
+                                <?php } ?>
+                            </tbody>
+                            <tfoot class="table-inverse">
+                                <td>Total</td>
                                 <td></td>
                                 <td></td>
                                 <td></td>
                                 <td></td>
                                 <td>= Rp<?php echo $order_info->order_total ?></td>
-                                </tfoot>
-                            </table>
-                        </div>
+                            </tfoot>
+                        </table>
                     </div>
                 </div>
             </div>
-            <div class="footer_area">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <p align="center">Copyright By Rostom Ali</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
         </div>
-    </body>
+        <div class="footer_area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p align="center"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+
 </html>
