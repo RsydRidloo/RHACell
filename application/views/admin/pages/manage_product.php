2fe5a0e0582729f8ff5233d7b350aa5b274f5d4e
Rasyid Ridlo <61292802+RsydRidloo@users.noreply.github.com>
2021-07-03 22:45:58 +0700

    Update manage_product.php

--- application/views/admin/pages/manage_product.php
+++ application/views/admin/pages/manage_product.php
@@ -5,97 +5,106 @@
     <ul class="breadcrumb">
         <li>
             <i class="icon-home"></i>
-            <a href="<?php echo base_url('dashboard')?>">Home</a> 
+            <a href="<?php echo base_url('dashboard') ?>">Home</a>
             <i class="icon-angle-right"></i>
         </li>
-        <li><a href="<?php echo base_url('manage/product')?>">Manage Product</a></li>
+        <li><a href="<?php echo base_url('manage/product') ?>">Kelola Produk</a></li>
     </ul>
 
-    <div class="row-fluid sortable">		
+    <div class="row-fluid sortable">
         <div class="box span12">
             <div class="box-header" data-original-title>
-                <h2><i class="halflings-icon user"></i><span class="break"></span>Manage Product</h2>
+                <h2><i class="halflings-icon user"></i><span class="break"></span>Kelola Produk</h2>
                 <div class="box-icon">
                     <a href="#" class="btn-setting"><i class="halflings-icon wrench"></i></a>
                     <a href="#" class="btn-minimize"><i class="halflings-icon chevron-up"></i></a>
                     <a href="#" class="btn-close"><i class="halflings-icon remove"></i></a>
                 </div>
             </div>
-            
+
             <style type="text/css">
-                #result{color:red;padding: 5px}
-                #result p{color:red}
+                #result {
+                    color: red;
+                    padding: 5px
+                }
+
+                #result p {
+                    color: red
+                }
             </style>
             <div id="result">
                 <p><?php echo $this->session->flashdata('message'); ?></p>
             </div>
-            
+
             <div class="box-content">
                 <table class="table table-striped table-bordered bootstrap-datatable datatable">
                     <thead>
                         <tr>
                             <th>Sr.</th>
-                            <th>Product Title</th>
-                            <th>Product Image</th>
-                            <th>Product Price</th>
-                            <th>Product Qty</th>
-                            <th>Publication Status</th>
-                            <th>Actions</th>
+                            <th>Judul Produk</th>
+                            <th>Gambar Produk</th>
+                            <th>Harga Produk</th>
+                            <th>Stok Produk</th>
+                            <th>Status Publikasi</th>
+                            <th>Aksi</th>
                         </tr>
-                    </thead>   
+                    </thead>
                     <tbody>
-                        <?php 
-                        $i=0;
-                        foreach($get_all_product as $single_product){
+                        <?php
+                        $i = 0;
+                        foreach ($get_all_product as $single_product) {
                             $i++;
                         ?>
-                        <tr>
-                            <td><?php echo $i;?></td>
-                            <td class="center"><?php echo $single_product->product_title;?></td>
-                            <td class="center"><img src="<?php echo base_url('uploads/'.$single_product->product_image);?>" style="width:200px;height:75px"/></td>
-                            <td class="center">Rp<?php echo $this->cart->format_number($single_product->product_price);?></td>
-                            <td class="center"><?php echo $single_product->product_quantity;?></td>
-                            <td class="center">
-                                <?php if ($single_product->pstatus == 1) { ?>
-                                    <span class="label label-success">Published</span>
-                                <?php } else {
+                            <tr>
+                                <td><?php echo $i; ?></td>
+                                <td class="center"><?php echo $single_product->product_title; ?></td>
+                                <td class="center"><img src="<?php echo base_url('uploads/' . $single_product->product_image); ?>" style="width:200px;height:75px" /></td>
+                                <td class="center">Rp<?php echo $this->cart->format_number($single_product->product_price); ?></td>
+                                <td class="center"><?php echo $single_product->product_quantity; ?></td>
+                                <td class="center">
+                                    <?php if ($single_product->pstatus == 1) { ?>
+                                        <span class="label label-success">Sudah di publish</span>
+                                    <?php } else {
                                     ?>
-                                    <span class="label label-danger" style="background:red">Unpublished</span>
+                                        <span class="label label-danger" style="background:red">Belum di publish</span>
                                     <?php }
-                                ?>
-                            </td>
-                           <td class="center">
+                                    ?>
+                                </td>
+                                <td class="center">
                                     <?php if ($single_product->pstatus == 0) { ?>
                                         <a class="btn btn-success" href="<?php echo base_url('published/product/' . $single_product->product_id); ?>">
-                                            <i class="halflings-icon white thumbs-up"></i>  
+                                            <i class="halflings-icon white thumbs-up"></i>
                                         </a>
                                     <?php } else {
-                                        ?>
+                                    ?>
                                         <a class="btn btn-danger" href="<?php echo base_url('unpublished/product/' . $single_product->product_id); ?>">
-                                            <i class="halflings-icon white thumbs-down"></i>  
+                                            <i class="halflings-icon white thumbs-down"></i>
                                         </a>
-                                        <?php }
+                                    <?php }
                                     ?>
 
                                     <a class="btn btn-info" href="<?php echo base_url('edit/product/' . $single_product->product_id); ?>">
-                                        <i class="halflings-icon white edit"></i>  
+                                        <i class="halflings-icon white edit"></i>
                                     </a>
                                     <a class="btn btn-danger" href="<?php echo base_url('delete/product/' . $single_product->product_id); ?>">
-                                        <i class="halflings-icon white trash"></i> 
+                                        <i class="halflings-icon white trash"></i>
                                     </a>
                                 </td>
-                        </tr>
-                        <?php }?>
-                        
+                            </tr>
+                        <?php } ?>
+
                     </tbody>
-                </table>            
+                </table>
             </div>
-        </div><!--/span-->
+        </div>
+        <!--/span-->
 
-    </div><!--/row-->
+    </div>
+    <!--/row-->
 
 
 
-</div><!--/.fluid-container-->
+</div>
+<!--/.fluid-container-->
 
 <!-- end: Content -->
