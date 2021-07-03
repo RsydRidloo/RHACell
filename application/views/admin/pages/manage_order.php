ddd8bbdd8ea6750da506fe7065da296875ba9010
Rivadhli16 <rivadli.alli@gmail.com>
2021-07-03 23:48:30 +0800

    Update manage_order.php

--- application/views/admin/pages/manage_order.php
+++ application/views/admin/pages/manage_order.php
@@ -5,13 +5,13 @@
     <ul class="breadcrumb">
         <li>
             <i class="icon-home"></i>
-            <a href="<?php echo base_url('dashboard')?>">Home</a> 
+            <a href="<?php echo base_url('dashboard') ?>">Home</a>
             <i class="icon-angle-right"></i>
         </li>
-        <li><a href="<?php echo base_url('manage/product')?>">Manage Product</a></li>
+        <li><a href="<?php echo base_url('manage/product') ?>">Manage Product</a></li>
     </ul>
 
-    <div class="row-fluid sortable">		
+    <div class="row-fluid sortable">
         <div class="box span12">
             <div class="box-header" data-original-title>
                 <h2><i class="halflings-icon user"></i><span class="break"></span>Manage Product</h2>
@@ -21,55 +21,64 @@
                     <a href="#" class="btn-close"><i class="halflings-icon remove"></i></a>
                 </div>
             </div>
-            
+
             <style type="text/css">
-                #result{color:red;padding: 5px}
-                #result p{color:red}
+                #result {
+                    color: red;
+                    padding: 5px
+                }
+
+                #result p {
+                    color: red
+                }
             </style>
             <div id="result">
                 <p><?php echo $this->session->flashdata('message'); ?></p>
             </div>
-            
+
             <div class="box-content">
                 <table class="table table-striped table-bordered bootstrap-datatable datatable">
                     <thead>
                         <tr>
-                            <th>Sr.</th>
-                            <th>Customer Name</th>
-                            <th>Customer Number</th>
-                            <th>Customer Phone</th>
-                            <th>Total Amount</th>
-                            <th>Actions</th>
+                            <th>No.</th>
+                            <th>Nama Pelanggan</th>
+                            <th>Nomor pelanggan</th>
+                            <th>Telepon Pelanggan</th>
+                            <th>Jumlah total</th>
+                            <th>Aksi</th>
                         </tr>
-                    </thead>   
+                    </thead>
                     <tbody>
-                        <?php 
-                        $i=0;
-                        foreach($all_manage_order_info as $single_order){
+                        <?php
+                        $i = 0;
+                        foreach ($all_manage_order_info as $single_order) {
                             $i++;
-                            ?>
-                        <tr>
-                            <td><?php echo $i;?></td>
-                            <td><?php echo $single_order->customer_name?></td>
-                            <td><?php echo $single_order->customer_phone?></td>
-                            <td><?php echo $single_order->customer_email?></td>
-                            <td>Rp<?php echo $this->cart->format_number($single_order->order_total)?></td>
-                            <td>
-                                <a class="btn btn-warning"><?php echo $single_order->actions?></a>
-                                <a class="btn btn-danger" href="<?php echo base_url('order/details/'.$single_order->order_id);?>">View</a>
-                                <a class="btn btn-success" href="<?php echo base_url('web/pdf/'.$single_order->order_id);?>">Download</a>
-                            </td>
-                        </tr>
-                        <?php }?>
+                        ?>
+                            <tr>
+                                <td><?php echo $i; ?></td>
+                                <td><?php echo $single_order->customer_name ?></td>
+                                <td><?php echo $single_order->customer_phone ?></td>
+                                <td><?php echo $single_order->customer_email ?></td>
+                                <td>Rp<?php echo $this->cart->format_number($single_order->order_total) ?></td>
+                                <td>
+                                    <a class="btn btn-warning"><?php echo $single_order->actions ?></a>
+                                    <a class="btn btn-danger" href="<?php echo base_url('order/details/' . $single_order->order_id); ?>">Lihat</a>
+                                    <a class="btn btn-success" href="<?php echo base_url('web/pdf/' . $single_order->order_id); ?>">Download</a>
+                                </td>
+                            </tr>
+                        <?php } ?>
                     </tbody>
-                </table>            
+                </table>
             </div>
-        </div><!--/span-->
+        </div>
+        <!--/span-->
 
-    </div><!--/row-->
+    </div>
+    <!--/row-->
 
 
 
-</div><!--/.fluid-container-->
+</div>
+<!--/.fluid-container-->
 
 <!-- end: Content -->
